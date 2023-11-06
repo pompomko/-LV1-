5bfe37049fa6d2ddef9b999d5c7a358c7809be1c
pompomko <sdw12334@gmail.com>
2023-11-06 19:50:28 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="App-header">오늘 할일</div>
+      <div className="input-box">
+        <div>
+          제목<input></input>
+          내용<input></input>
+        </div>
+
+        <button>추가하기</button>
+      </div>
+      <br />
+      <div className="todoList-box">
+        <div className="List">오늘 할일들</div>
+        <br />
+        <div className="todoList-grid">
+          <div className="todo">
+            <div className="todo-title">리액트 공부하기</div>
+            <br />
+            <div className="todo-detail">리액트 기초를 공부해보자</div>
+            <br />
+            <button className="deletebtn">삭제하기</button>
+            <button className="completebtn">완료!</button>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="complete-box">
+        <div className="List">할일 완료!</div>
+        <br />
+        <div className="todoList-grid">
+          <div className="todo">
+            <div className="todo-title">리액트 공부하기</div>
+            <br />
+            <div className="todo-detail">리액트 기초를 공부해보자</div>
+            <br />
+            <button className="deletebtn">삭제하기</button>
+            <button className="completebtn">완료!</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
